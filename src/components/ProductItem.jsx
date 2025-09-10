3e004eb94c8f82c93ff5cc5f98fc6547e1446c26
vjbravo123 <vjbravojoshi@gmail.com>
2025-09-10 20:18:10 +0530

    ProductItem dispatcher

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,6 +1,9 @@
+import { useDispatch } from "react-redux";
+import { addToCart } from "../store/cartSlice";
 import { Link } from "react-router-dom";
 
 const ProductItem = ({ product }) => {
+  const dispatch = useDispatch();
 
   return (
     <div className="bg-white rounded-xl shadow hover:shadow-lg transition p-4 flex flex-col">
@@ -15,6 +18,7 @@ const ProductItem = ({ product }) => {
         <p className="text-blue-600 font-bold mt-1">${product.price}</p>
       </Link>
       <button
+        onClick={() => dispatch(addToCart(product))}
         className="mt-3 bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700 transition"
       >
         Add to Cart
