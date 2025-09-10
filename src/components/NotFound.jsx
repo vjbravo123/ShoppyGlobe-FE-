37bf923dc76b7881c2e7797318ff2480503ac130
vjbravo123 <vjbravojoshi@gmail.com>
2025-09-10 20:35:41 +0530

    notfound page added

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
-  )
-}
+    <div className="flex flex-col items-center justify-center h-[70vh] text-center">
+      <h1 className="text-6xl font-bold text-red-600">404</h1>
+      <p className="text-xl mt-4">Oops! The page you are looking for does not exist.</p>
+      <Link
+        to="/"
+        className="mt-6 bg-blue-600 text-white px-6 py-2 rounded-md hover:bg-blue-700"
+      >
+        Go Back Home
+      </Link>
+    </div>
+  );
+};
 
-export default NotFound
+export default NotFound;
